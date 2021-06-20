aca5dd8158ef25f09d77e2ba493427f8a0409489
Andrew Waite <apwaite86@gmail.com>
2021-06-20 01:38:46 +0100

    Added functionality that allows players to mark X or O onto the grid. I'm confident there's a better way of going about this but it does appear to do the job. I'm playing with the idea of having the game-status and instructions panels being managed by the displayController as this seems like the waay to go, but I would like to do more with the Player factory function as right now it doesn't appear to be doing much.

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 const gameBoard = (() => {
   "use strict";
 
-  let _currentPlayer = "X";
-  let gameboard = ["X", "X", "O", "X", "X", "X", "O", "O", "O"];
+  let gameboard = ["", "", "", "", "", "", "", "", ""];
 
   // 0,1,2,
   // 3,4,5,
@@ -12,7 +11,6 @@ const gameBoard = (() => {
 
   function _logInfo() {
     console.log(gameboard);
-    console.log(_currentPlayer);
   }
 
   function reportInfo() {
@@ -25,7 +23,7 @@ const gameBoard = (() => {
     for (let i = 0; i < array.length; i++) {
       let element = document.querySelector(".gameboard");
       let newElement = document.createElement("section");
-      newElement.classList.add(`cell`, `cell-${[i]}`);
+      newElement.classList.add(`cell`, `cell-${[i]}`, `${[i]}`);
       if (array[i].includes("X") || array[i].includes("O")) {
         newElement.textContent = array[i];
       }
@@ -43,16 +41,67 @@ const gameBoard = (() => {
 const displayController = (() => {
   "use strict";
 
+  let currentPlayer = "X";
+
+  const instructions = document.querySelector(".instructions");
+
+  function showPlayer() {
+    const playerData = document.querySelectorAll(".game-status > p");
+    if (currentPlayer === "X") {
+      // todo: add text-decoration underline
+      // playerData[0].textContent = "Player (X)";
+    } else if (currentPlayer === "O") {
+      // todo: add text-decoration underline
+      // playerData[1].textContent = "Player (O)";
+    }
+    instructions.textContent = `Your move, ${currentPlayer}!`;
+  }
+
+  function _logInfo() {
+    console.log(currentPlayer);
+  }
+
+  function reportInfo() {
+    _logInfo();
+  }
+
   gameBoard.renderGameBoard(gameBoard.gameboard);
-  return {};
+
+  // todo: logic that determines whether a spot can be marked by a player
+
+  document.querySelectorAll(".cell").forEach((item) => {
+    item.addEventListener("click", (event) => {
+      let selected = event.target.classList[2];
+      if (gameBoard.gameboard[`${selected}`] === "") {
+        event.target.textContent = `${currentPlayer}`;
+        gameBoard.gameboard[`${selected}`] = `${currentPlayer}`;
+        console.log(selected);
+      } else if (
+        event.target.textContent === "O" ||
+        event.target.textContent === "X"
+      ) {
+        console.log("Error, choose another area");
+      }
+      if (currentPlayer === "X") {
+        currentPlayer = "O";
+      } else if (currentPlayer === "O") {
+        currentPlayer = "X";
+      }
+      showPlayer();
+      console.log("Checking for win condition....");
+      console.log(currentPlayer);
+    });
+  });
+
+  return {
+    reportInfo: reportInfo,
+    currentPlayer: currentPlayer,
+    showPlayer: showPlayer,
+  };
 })();
 
 const Player = (name) => {
-  const instructions = document.querySelector(".instructions");
-
-  //todo: create a function that
-
-  const showPlayer = () => (instructions.textContent = `Your move, ${name}!`);
+  const showPlayer = () => displayController.showPlayer(name);
   return { showPlayer };
 };
 
@@ -63,3 +112,4 @@ const playerTwo = Player("O");
 playerOne.showPlayer();
 playerTwo.showPlayer();
 gameBoard.reportInfo();
+displayController.reportInfo();
